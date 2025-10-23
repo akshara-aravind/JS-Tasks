1aa0f0e854ca36168279f702e373c5b3dd308aae
akshara-aravind <akshara.p@vonnue.com>
2025-10-23 21:27:32 +0530

    progress scroll bar

---
+++ Day6/Task8.js
@@ -0,0 +1,82 @@
+// 8. Create a small Employee Manager CLI app using Node.js
+/**
+Accepts input from the terminal (readline or prompt-sync)
+
+Add employee (name, age, role)
+
+List all employees
+
+Delete an employee by name 
+ */
+const readline = require('readline'); // help read input from terminal
+
+const rl = readline.createInterface({
+    input:process.stdin,  // read from input 
+    output:process.stdout // write in terminal
+});
+
+let employees = [];
+
+function showMenu(){
+    console.log(`____ Employee Manager ____
+        1. Add Employee
+        2. List Employee
+        3. Delete Employee
+        4. Exit
+        `);
+        rl.question('Choose an option (1-4): ', handleMenu);
+}
+
+function handleMenu(choice){
+    switch(choice.trim()){
+        case '1':
+            addEmployee();
+            break;
+        case '2' :
+            listEmployee();
+            break;
+        case '3' :
+            deleteEmployee();
+            break;
+        case '4' :
+            console.log('Goodbye!');
+            rl.close();
+            break;
+        default :
+        console.log('Invalid choice. Try again.')
+        showMenu();
+    }
+}
+
+function addEmployee(){
+    rl.question('Enter name: ', (name) => {
+        rl.question('Enter age: ', (age) =>{
+            rl.question('Enter role: ', (role) => {
+                employees.push({name,age,role})
+                console.log(`Added ${name} successfully`);
+                showMenu();
+            });
+        });
+    });
+}
+
+function listEmployee(){
+    if(employees.length === 0){
+        console.log('No employees found.');
+    }else{
+        console.log('\n---Employee List---');
+        employees.forEach((emp,i) => {
+            console.log(`${i+1}.${emp.name} | Age: ${emp.age} | ${emp.role}`);
+        });
+    }
+    showMenu();
+}
+
+function deleteEmployee(){
+    rl.question('Enter employee name to delete: ' ,(name) => {
+      const index = employees.findIndex((e) => e.name.toLowerCase() === name.toLowerCase())
+      if(index !== -1){
+        
+      }
+    })
+}
--- Day7/Task7/task7.js
+++ Day7/Task7/task7.js
@@ -1,5 +1,9 @@
 //7. Create a container with random text and should be scrollable. Show a progress bar below, that fills as the user scrolls down the page.
 const color =document.querySelector('.color')
-// window.addEventListener('scroll',() =>{
-// color.style.backgroundColor = 
-// })
+window.addEventListener('scroll',() =>{
+const scrollTop = document.documentElement.scrollTop;
+const scrollHeight = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+
+const percent = (scrollTop / scrollHeight) * 100
+color.style.width = percent + '%'
+})
