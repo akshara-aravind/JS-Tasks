7bf6d72b9bef5befe50018d4cb891ffa7ed06d73
akshara-aravind <akshara.p@vonnue.com>
2025-11-08 22:23:32 +0530

    Nov8

--- Day15/task4.js
+++ Day15/task4.js
@@ -1,21 +1,29 @@
 //4. Write a function which takes two integers n and k, return all possible combinations of k numbers chosen from the range [1, n].
+
 function combinations(n,k){
-    let result = []
-    let num = []
-    for(let i=1; i<=n; i++){
-     num.push(i)
+    if(typeof n !== 'number' || typeof k !== 'number' || k <= 0, n <= 0) return 'Invalid'
+
+    let nums =[]
+    for(let i=0; i<=n ; i++){
+        nums.push(i)
     }
-    if(n === 1){
-        result.push(num)
+    let result = [[]]
+
+    for(let i=0; i< nums.length; i++){
+        let newCombns = []
+        for(let item of result){
+            let next = [...item, nums[i]]
+            if(next.length <= k){
+                newCombns.push(next)
+            }
+        }
+        result.push(...newCombns)
     }
-    if( n <= 0) return 'Invalid'
-     for(let i=0; i< num.length;i++){
-    // console.log(num[i])
-    for(let j=i+1;j<num.length;j++){
-        result.push([num[i],num[j]])
-       }
-     }
-    return result
+result =  result.filter(el => el.length === k)
+
+result = result.filter(arr => arr.every(num => num >= 1 && num <= n ))
+
+return result
 }
 console.log(combinations(4,2))
 console.log(combinations(1,1))
